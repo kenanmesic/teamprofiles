781e28ceaecf8318ec4ecacc72dea96b7ee7f603
kenan <kenanmesic1191@gmail.com>
2023-01-26 22:05:49 -0800

    reworking index

---
+++ index.js
@@ -0,0 +1,12 @@
+const employee = require ("./lib/employee")
+
+const newEmployee = new employee("alex", 20, "lex@alex.com")
+
+console.log(newEmployee.getName())
+
+//ask what you want to do? your prompts shoudl then assign things like role iddepartment etc.
+//make anew employee?
+//if employee who do they work for etc
+//if manager what department
+// create constructors files for employees and managers and interns
+//some html markup generated with teh teams etc
